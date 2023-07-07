6d00c4fd0eb359a6f396e940c5bfa70bfd851b9b
mainug <alsdnr0850@gmail.com>
2023-07-07 09:31:51 +0000

    1weak

---
+++ 27323/problem_code.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class problem_code {
+    Scanner in = new Scanner(System.in);
+    int A = in.nextInt();
+    int B = in.nextInt();
+
+    int area = A * B;
+
+    System.out.println(area);
+
+    in.close();
+}
